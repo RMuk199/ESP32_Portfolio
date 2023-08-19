9fadf7865829b96385150bcdb7940ffba5dad327
RMuk199 <140573289+RMuk199@users.noreply.github.com>
2023-08-19 13:13:05 +1000

    Update Blink_Demo.ino

--- Blink_Demo.ino
+++ Blink_Demo.ino
@@ -1,12 +1,17 @@
 void setup() {
   // put your setup code here, to run once:
+  //COnnect to GPIO pin 2
   pinMode(2, OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  //Send high energy to led through pin 2 (turn it on)
   digitalWrite(2, HIGH);
+  //Wait 500 miliseconds
   delay(500);
+  //send low energy/stop energy to led (turn off)
   digitalWrite(2, LOW);
+  //Wait 500 ms
   delay(500);
 }
